aca48d83f4bc53713b1ae8a8af0ccec28a6a2d11
ryuu <ryuu.public@gmail.com>
2023-03-02 17:39:43 +0900

    first commit

---
+++ src/main/java/com/selurba/abrules/AbrulesApplication.java
@@ -0,0 +1,13 @@
+package com.selurba.abrules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbrulesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbrulesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/selurba/abrules/AbrulesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selurba.abrules;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbrulesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
