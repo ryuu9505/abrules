c9e42a7b13738b32bfef4f7bfbd352d467d417ca
ryuu <ryuu.public@gmail.com>
2023-03-02 19:33:34 +0900

    test: modify 'BusinessRuleEngine', 'Action' logics  in the existing test cases to receive the 'Facts' instance as conditions for execution.

--- src/test/java/com/selurba/abrules/BusinessRuleEngineTest.java
+++ src/test/java/com/selurba/abrules/BusinessRuleEngineTest.java
@@ -9,29 +9,44 @@ class BusinessRuleEngineTest {
 
     @Test
     void shouldHaveNoRulesInitially() {
-        final BusinessRuleEngine engine = new BusinessRuleEngine();
+        final Facts mockFacts = mock(Facts.class);
+        final BusinessRuleEngine engine = new BusinessRuleEngine(mockFacts);
 
         assertEquals(0, engine.count());
     }
 
     @Test
     void shouldAddTwoActions() {
-        final BusinessRuleEngine engine = new BusinessRuleEngine();
+        final Facts mockFacts = mock(Facts.class);
+        final BusinessRuleEngine engine = new BusinessRuleEngine(mockFacts);
 
-        engine.addAction(() -> {});
-        engine.addAction(() -> {});
+        engine.addAction(facts -> {
+            final String jobTitle = facts.getFact("jobTitle");
+            if("CEO".equals(jobTitle)) {
+                final String name = facts.getFact("name");
+                //Mailer.sendEmail("sales@company.com", "Relevant customer: " + name);
+            }
+        });
+        engine.addAction(facts -> {
+            final String jobTitle = facts.getFact("jobTitle");
+            if("President".equals(jobTitle)) {
+                final String name = facts.getFact("name");
+                //Mailer.sendEmail("sales@company.com", "Relevant customer: " + name);
+            }
+        });
 
         assertEquals(2, engine.count());
     }
 
     @Test
-    void shouldExecuteOneAction() {
-        final BusinessRuleEngine engine = new BusinessRuleEngine();
+    void shouldExecuteAnActionWithFacts() {
         final Action mockAction = mock(Action.class);
+        final Facts mockFacts = mock(Facts.class);
+        final BusinessRuleEngine engine = new BusinessRuleEngine(mockFacts);
 
         engine.addAction(mockAction);
         engine.run();
 
-        verify(mockAction).execute();
+        verify(mockAction).execute(mockFacts);
     }
 }
